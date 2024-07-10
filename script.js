90f617f8f2fcaeb7cca2c89ccd047a93997fa7c6
AndreaSdr70 <andrea@sdringola.it>
2024-07-10 23:52:39 +0200

    Selfwork Variabili - corretto

---
+++ script.js
@@ -0,0 +1,27 @@
+let num1 = 5;
+
+let num2 = 10;
+
+console.log( " il valore della variabile num1 è " + num1);
+
+console.log( " il valore della variabile num2 è " + num2);
+
+let str = "pietra";
+
+console.log( " Il valore della variabile stringa è " + str);
+
+str = "pluto";
+
+console.log( " Il valore della variabile stringa è " + str);
+
+const PGRECO = 3.14159265359;
+
+console.log( " Il valore di Pigreco è " + PGRECO);
+
+// const PGRECO = 5; messo cosi altrimenti da errore
+
+
+
+
+
+
